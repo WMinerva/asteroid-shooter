785bc0154893d12e45908ea2426f107e95ebb4f4
WMinerva <nicolas.ochoa1697@gmail.com>
2026-01-23 23:58:27 -0400

    laser queue free

--- scenes/laser/laser.gd
+++ scenes/laser/laser.gd
@@ -13,3 +13,7 @@ func _on_area_entered(area: Area2D) -> void:
 	#pass # Replace with function body.
 	if area.is_in_group("asteroids"):
 		queue_free()
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()	
